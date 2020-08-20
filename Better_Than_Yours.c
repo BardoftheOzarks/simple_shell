8870ce23962148004d7dcddaebe0306844529e68
BardoftheOzarks <1886@holbertonschool.com>
2020-08-20 02:07:58 +0000

    cleaning/reformatting

--- Better_Than_Yours.c
+++ Better_Than_Yours.c
@@ -4,83 +4,49 @@
 #include <stdio.h>
 
 /**
-* main - function
-* description: print
-* @ac: lists
-* @av: stuff
-* Return: size
+* main - It's a shell
+* Return: Nothing, it's an empty shell
 */
-
-
-
-
-
 int main(void)
 {
-    int status = 1;
-    size_t size = 0;
-    char *buf = NULL;
-    char *prompt = "Sea-Shell>> ";
-    char *arg[10];
-    int str = 0;
-    pid_t pid;
-    char *fullcmd;
-    char *slash = "/";
-    char *path;
-    char *cmd; 
+	int status, len;
+	size_t size = 0;
+	char *buf = NULL, *arg[10], *fullcmd, *path, *cmd;
+	pid_t pid;
 
-while (status == 1)
-{
-    write(STDOUT_FILENO, prompt, 12);
-    getline(&buf, &size, stdin);
+	while (1)
+	{
+		print("Sea-Shell$ ");
+		status = getline(&buf, &size, stdin);
+		if (status == -1)
+			break;
+		len = _strlen(buf);
+		buf[len - 1] = '\0';
+		if (_strcmp(buf, "exit") == 0)
+			break;
+		path = env_path(buf);
+		if (path == '\0' || path == NULL)
+		{
+			free(buf);
+			break;
+		}
+		cmd = buf;
 
+		fullcmd = strcat(path, "/");
+		fullcmd = strcat(fullcmd, cmd);
 
-
-    str = strlen(buf);
-    buf[str - 1] = '\0';
-    
-    
-    path = env_path(buf);
-    if (path == '\0' || path == NULL)
-    {
-        free(buf);
-        break;
-    }
-    cmd = buf;
-          
-    fullcmd = strcat(path, slash);
-    fullcmd = strcat(fullcmd, cmd);
-    
-    pid = fork();
-    if (pid == 0)
-    {
-        arg[0] = "Hello";
-        arg[1] = '\0';
-        execve (fullcmd, arg, environ);
-        exit(0);
-    }
-    
-    else
-    {
-        free(buf);
-        wait (NULL);
-        
-    }
-
-
-
-    if (strcmp (buf, "exit") == 0 )
-    {
-        free (buf);
-        return (0);
-    }
-
-    if (feof(stdin))
-    {
-        free (buf);
-        return(0);
-    }
-    continue;
-}
-return (0);
+		pid = fork();
+		if (pid == 0)
+		{
+			arg[0] = "Hello";
+			arg[1] = '\0';
+			execve(fullcmd, arg, environ);
+			exit(0);
+		} else
+		{
+			free(buf);
+			wait(NULL);
+		}
+	}
+	return (0);
 }
