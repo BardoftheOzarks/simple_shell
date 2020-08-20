497a3df3e0360d628e72e86d4a68b8977a259d5a
BardoftheOzarks <1886@holbertonschool.com>
2020-08-20 03:32:52 +0000

    bettyfied

--- Better_Than_Yours.c
+++ Better_Than_Yours.c
@@ -11,8 +11,7 @@ int main(void)
 {
 	int status, len, i;
 	size_t size = 0;
-	char *buf = NULL, **av = malloc(sizeof(char *) * ARG_MAX);
-	char *fullcmd, *path, *cmd;
+	char *buf = NULL, **av = malloc(sizeof(char *) * ARG_MAX), *full, *path;
 	pid_t pid;
 
 	while (1)
@@ -20,44 +19,32 @@ int main(void)
 		if (isatty(0))
 			print("Sea-Shell$ ");
 		status = getline(&buf, &size, stdin);
-
 		if (status == -1)
 			break;
 		len = _strlen(buf);
 		buf[len - 1] = '\0';
 		if (_strcmp(buf, "exit") == 0)
 			break;
-
 		i = 0;
 		av[i] = strtok(buf, " "); /*write func _strtok*/
-                while (av[i] && i <= ARG_MAX)
-                        av[++i] = strtok(NULL, " ");
-                av[i] = NULL;
-
+		while (av[i] && i <= ARG_MAX)
+			av[++i] = strtok(NULL, " ");
+		av[i] = NULL;
 		path = env_path(buf);
 		if (path == '\0' || path == NULL)
-		{
-			free(buf);
-			break;
-		}
-		cmd = buf;
-
-		fullcmd = _strcat(path, "/");
-		fullcmd = _strcat(fullcmd, cmd);
-
+		{	free(buf);
+			break; }
+		full = _strcat(path, "/");
+		full = _strcat(full, av[0]);
 		pid = fork();
 		if (pid == 0)
-		{
-			execve(fullcmd, av, environ);
+		{	execve(full, av, environ);
 			exit(0);
 		} else
-		{
-			free(buf);
+		{	free(buf);
 			buf = NULL;
 			free(path);
 			path = NULL;
-			wait(NULL);
-		}
-	}
+			wait(NULL); }}
 	return (0);
 }
