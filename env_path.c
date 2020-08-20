77f9345f03f5e578451d4cab8a217ea9efad5231
BardoftheOzarks <1886@holbertonschool.com>
2020-08-20 01:53:04 +0000

    bettied env_path

--- env_path.c
+++ env_path.c
@@ -8,47 +8,44 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <errno.h>
-
-
-
+/**
+ * env_path - gets the path
+ * @cmd: simple command
+ * Return: string with full path
+ */
 char *env_path(char *cmd)
 {
-    int j = 0;
-    char *path;
-    const char de[2] = "=";
-    const char ds[2] = ":";
-    char *dir;
-    DIR *dp;
-    struct dirent *entry;
+	int j = 0;
+	char *path;
+	const char de[2] = "=";
+	const char ds[2] = ":";
+	char *dir;
+	DIR *dp;
+	struct dirent *entry;
 
+	while (environ[j] != NULL)
+	{
+		if (strncmp("PATH", environ[j], 3) == 0)
+		{
+			path = environ[j];
+			dir = strtok(path, de);
 
-
-while (environ[j] != NULL)
-{
-    if (strncmp("PATH", environ[j], 3) == 0)
-    {
-        path = environ[j];
-        dir = strtok(path, de);
-        
-         while (dir != NULL)
-        {
-            dir = strtok(NULL, ds);            
-            dp = opendir(dir);
-            while ((entry = readdir(dp)) != NULL)
-            {
-                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-                    continue;
-                if (strcmp(cmd, entry->d_name) == 0)
-                    {
-                        return(dir);
-                    }
-                    continue;
-            }
-            closedir(dp);
-        }
-        return(0);
-    }
-    j++;
-}
-return (0);
+			while (dir != NULL)
+			{
+				dir = strtok(NULL, ds);
+				dp = opendir(dir);
+				while ((entry = readdir(dp)) != NULL)
+				{
+					if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+						continue;
+					if (strcmp(cmd, entry->d_name) == 0)
+						return(dir);
+				}
+				closedir(dp);
+			}
+			return(0);
+		}
+		j++;
+	}
+	return (0);
 }
