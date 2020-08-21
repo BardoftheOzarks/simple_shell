ede44257e0a31a03ebaf47b56592bd5241aa8db3
Justin4587 <Thurman@atlasok.com>
2020-08-21 10:47:54 -0500

    interactive

--- buf_splitter.c
+++ buf_splitter.c
@@ -28,11 +28,20 @@ char **buf_splitter(char *buf)
 		av[i] = NULL;
 		return (av);	}
 	i = 0;
-	av[i] = strtok(buf, " "); /*write func _strtok*/
-	cmd = av[0];
-	while (av[i] && i <= ARG_MAX)
-		av[++i] = strtok(NULL, " ");
-	av[i] = '\0';
+	
+    temp = strtok(buf, " ");
+    av[i] = _strdup(temp);
+	cmd = av[i];
+    i++;
+	while (i < 10)
+    {
+		temp = strtok(NULL, " ");
+        if (temp == NULL)
+            break;
+        av[i] = _strdup(temp);
+        i++;
+    }
+    av[i] = NULL;
 	av[0] = env_path(av[0]);
 	if (av[0] == '\0' || av[0] == NULL)
 	{	free_grid(&av);
@@ -43,5 +52,7 @@ char **buf_splitter(char *buf)
 	free_ptr = av[0];
 	av[0] = _strcat(av[0], cmd);
 	free(free_ptr);
+    free(cmd);
+    free(temp);
 	return (av);
 }
